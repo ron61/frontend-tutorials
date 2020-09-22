af741ff90b51a43c1f1ae0d294aae559107d4082
ron61 <ron233f@gmail.com>
2020-09-22 22:08:10 +0900

    vue start

---
+++ MDN/Vue/moz-todo-vue/src/components/ToDoItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" :checked="isDone" />
+    <label :for="id">{{label}}</label>
+  </div>
+</template>
+
+<script>
+import uniqueId from 'lodash.uniqueid';
+
+export default {
+  props: {
+    label: {
+      required: true,
+      type: String
+    },
+    done: {
+      default: false,
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      isDone: this.done,
+      id: uniqueId('todo-')
+    }
+  }
+}
+</script>
