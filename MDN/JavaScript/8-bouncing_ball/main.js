36638950687d9b5dbf871aea0e3ef2abfe24482c
ron61 <ron233f@gmail.com>
2020-09-22 16:43:27 +0900

    ball update done

--- MDN/JavaScript/8-bouncing_ball/main.js
+++ MDN/JavaScript/8-bouncing_ball/main.js
@@ -1,6 +1,7 @@
 // setup canvas
 
 const canvas = document.querySelector('canvas');
+const para = document.querySelector('p');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
@@ -13,16 +14,30 @@ function random(min, max) {
   return num;
 }
 
+// constructor of Shape
+function Shape(x, y, velX, velY, exists) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.exists = exists;
+}
+
 // constructor of balls
 
-function Ball(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
+function Ball(x, y, velX, velY, color, size, exists) {
+  Shape.call(this,x,y,velX,velY,exists);
+  this.color = color;
+  this.size = size;
 }
+
+Ball.prototype = Object.create(Shape.prototype);
+
+Object.defineProperty(Ball.prototype, 'constructor', {
+  value: Ball,
+  enumerable: false,
+  writable: true
+});
 
 Ball.prototype.draw = function() {
     ctx.beginPath();
@@ -68,25 +83,114 @@ Ball.prototype.collisionDetect = function() {
   }
 }
 
-// manage animation
+// constructor of evil
+
+function EvilCircle(x,y,size) {
+  Shape.call(this,x,y,20,20,true);
+  this.color = 'white';
+  this.size = size;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+
+Object.defineProperty(EvilCircle.prototype, 'constructor', {
+  value: EvilCircle,
+  enumerable: false,
+  writable: true
+});
+
+EvilCircle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function() {
+  if ((this.x + this.size) >= width) {
+    this.x = width - this.size;
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.x = this.size;
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.y = height - this.size;
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.y = this.size;
+  }
+  
+}
+
+EvilCircle.prototype.setControls = function() {
+  let _this = this;
+  window.onkeydown = function(e) {
+    if (e.key === 'a') {
+      _this.x -= _this.velX;
+    } else if (e.key === 'd') {
+      _this.x += _this.velX;
+    } else if (e.key === 'w') {
+      _this.y -= _this.velY;
+    } else if (e.key === 's') {
+      _this.y += _this.velY;
+    }
+  }
+}
+
+EvilCircle.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (balls[j].exists === true) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+        counter--;
+      }
+    }
+  }
+}
+
+const ballNumber = 2000;
+
+// ballをカウントする変数を初期化
+let counter = ballNumber;
+
+// ボールを生成
 
 let balls = [];
 
-while (balls.length < 10) {
+while (balls.length < ballNumber) {
   let size = random(5,10);
   let ball = new Ball(
     // ball position always drawn at least one ball width
     // away from the edge of the canvas, to avoid drawing errors
     random(0 + size,width - size),
     random(0 + size,height - size),
-    random(-10,10),
-    random(-10,10),
+    random(-1,1),
+    random(-1,1),
     'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-    size
+    size,
+    true
   );
 
   balls.push(ball);
 }
+
+// evilを生成
+evilSize = 100;
+evil = new EvilCircle(
+  random(0 + evilSize,width - evilSize),
+  random(0 + evilSize,height - evilSize),
+  evilSize
+);
+
+evil.setControls();
 
 // loop関数
 
@@ -97,16 +201,23 @@ function loop() {
     ctx.fillRect(0, 0, width, height);
   
     for (let i = 0; i < balls.length; i++) {
-      balls[i].draw();
-      balls[i].update();
-      balls[i].collisionDetect();
+      if(balls[i].exists === true) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+      }
     }
+
+    // evil
+    evil.draw();
+    evil.checkBounds();
+    evil.collisionDetect();
+
+    // paraのupdate
+    para.textContent = `Ball count : ${counter}`; 
     
     // 再帰的に呼び出すことでループさせる
     requestAnimationFrame(loop);
 }
 
-
-
 loop();
-
